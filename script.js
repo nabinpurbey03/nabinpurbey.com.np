5a46ed4a1c34522889564e0296afcfee642ed343
nabinpurbey03 <nabinpurbey03@hotmail.com>
2024-02-04 22:32:48 +0545

    added simple animations

--- script.js
+++ script.js
@@ -9,35 +9,41 @@ function pageLoader(path) {
 }
 
 const navLinks = document.querySelectorAll(".nav-bar ul li");
-// const activeLink = document.querySelectorAll(".active");
-
-navLinks.forEach(val => console.log(val));
-
 
 function loadAbout() {
     pageLoader("pages/about/about.html");
     navLinks.forEach(val => val.classList.remove("active"));
     navLinks[1].classList.add("active");
+    navLinks.forEach(val => val.classList.remove("puff-in-center"));
+    navLinks[1].classList.add("puff-in-center");
 }
 
 function loadResume() {
     pageLoader("pages/resume/resume.html");
     navLinks.forEach(val => val.classList.remove("active"));
     navLinks[2].classList.add("active");
+    navLinks.forEach(val => val.classList.remove("puff-in-center"));
+    navLinks[2].classList.add("puff-in-center");
 }
 
 function loadPortfolio() {
     pageLoader("pages/portfolio/portfolio.html");
     navLinks.forEach(val => val.classList.remove("active"));
     navLinks[3].classList.add("active");
+    navLinks.forEach(val => val.classList.remove("puff-in-center"));
+    navLinks[3].classList.add("puff-in-center");
 }
 
 function loadContact() {
     pageLoader("pages/contact/contact.html");
     navLinks.forEach(val => val.classList.remove("active"));
     navLinks[4].classList.add("active");
+    navLinks.forEach(val => val.classList.remove("puff-in-center"));
+    navLinks[4].classList.add("puff-in-center");
 }
 
 function reloadPage() {
     window.location.reload();
+    navLinks.forEach(val => val.classList.remove("puff-in-center"));
+    navLinks[0].classList.add("puff-in-center");
 }
