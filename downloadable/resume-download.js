c310458b3efa3b6ee5f357e8e3df0bbb83c8b336
nabinpurbey03 <nabinpurbey03@hotmail.com>
2024-02-07 23:45:17 +0545

    Pending js code

--- downloadable/resume-download.js
+++ downloadable/resume-download.js
@@ -1,13 +1,13 @@
-const name = document.getElementById("name").value;
+const name = document.getElementById("name").in;
 const email = document.getElementById("email").value;
 const desc = document.getElementById("reason").value;
 const nameErr = document.getElementById("nameErr");
 const emailErr = document.getElementById("emailErr");
 const descErr = document.getElementById("descErr");
 
-function validation(){
-    if(!name.contains(" ") === " "){
-        nameErr.innerHTML = "Please enter Firstname and Last name.";
-    }
+function validation() {
+    console.log(name)
+    console.log(email)
+    console.log(desc)
 }
 
