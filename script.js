c8674d2d96e849506b0acce4a57146ce8f3d4332
nabinpurbey03 <nabinpurbey03@hotmail.com>
2024-02-15 13:49:20 +0545

    updated download page

--- script.js
+++ script.js
@@ -8,30 +8,40 @@ function pageLoader(path) {
     });
 }
 
+function modifyState(url) {
+    window.history.pushState({ id: "2" }, "", url);
+}
+
 const navLinks = document.querySelectorAll(".nav-bar ul li");
 
 function loadAbout() {
     pageLoader("pages/about/about.html");
     navLinks.forEach(val => val.classList.remove("active"));
     navLinks[1].classList.add("active");
+    modifyState("about");
 }
 
 function loadResume() {
     pageLoader("pages/resume/resume.html");
     navLinks.forEach(val => val.classList.remove("active"));
     navLinks[2].classList.add("active");
+    modifyState("resume");
+
 }
 
 function loadPortfolio() {
     pageLoader("pages/portfolio/portfolio.html");
     navLinks.forEach(val => val.classList.remove("active"));
     navLinks[3].classList.add("active");
+    modifyState("portfolio");
+
 }
 
 function loadContact() {
     pageLoader("pages/contact/contact.html");
     navLinks.forEach(val => val.classList.remove("active"));
     navLinks[4].classList.add("active");
+    modifyState("contact");
 }
 
 function reloadPage() {
